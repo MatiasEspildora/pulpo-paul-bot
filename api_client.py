a2aab84a6ce4c1f4f7692a03e56f2fe88d8524ed
MatiasEspildora <matiasespildora@gmail.com>
2026-07-10 22:07:13 -0400

    Add generic API client and FootballAPI implementation
    Implemented a generic API client interface and a FootballAPI class for fetching data from the Football API.

---
+++ api_client.py
@@ -0,0 +1,25 @@
+import requests
+from abc import ABC, abstractmethod
+
+# Interfaz genérica para clientes de API
+class DataClient(ABC):
+    @abstractmethod
+    def get_data(self, endpoint, params):
+        pass
+
+class FootballAPI(DataClient):
+    def __init__(self, key):
+        self.base_url = "https://v3.football.api-sports.io"
+        self.headers = {
+            "x-rapidapi-key": key, 
+            "x-rapidapi-host": "v3.football.api-sports.io"
+        }
+
+    def get_data(self, endpoint, params):
+        try:
+            response = requests.get(f"{self.base_url}/{endpoint}", headers=self.headers, params=params)
+            response.raise_for_status() # Lanza error si el status no es 200
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"❌ Error conectando a API Football: {e}")
+            return None
