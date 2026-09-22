ed2fef73bc8b3411d74ce2f4f3a9fe95ee272e25
MatiasEspildora <matiasespildora@gmail.com>
2026-09-22 19:41:05 -0300

    Fix calculation of expected away score

--- analyzer.py
+++ analyzer.py
@@ -387,7 +387,7 @@ class MatchAnalyzer:
         away_avg_conceded = (ag_c_glob + ag_c_ven) / 2
 
         exp_home_score = (home_avg_scored + away_avg_conceded) / 2
-        exp_away_score = (away_avg_scored + home_concede_avg) / 2
+        exp_away_score = (away_avg_scored + home_avg_conceded) / 2
         total_projected_points = exp_home_score + exp_away_score
 
         diff = exp_home_score - exp_away_score
